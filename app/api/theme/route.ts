912b0e24a017ae075931e7d6924b59d4496668c0
azhmifauzimahdin <azhmifauzi11@gmail.com>
2025-06-19 19:44:56 +0700

    Repair: Post theme

--- app/api/theme/route.ts
+++ app/api/theme/route.ts
@@ -5,17 +5,21 @@ export async function POST(req: Request) {
   try {
     const body = await req.json();
 
-    const { name } = body;
+    const { name, originalPrice, discount } = body;
 
     const errors = [];
     if (!name) errors.push({ field: "name", message: "Nama harus diisi." });
+    if (!originalPrice)
+      errors.push({ field: "originalPrice", message: "Harga harus diisi." });
+    if (!discount)
+      errors.push({ field: "discount", message: "Diskon harus diisi." });
 
     if (errors.length > 0) {
       return NextResponse.json({ errors }, { status: 400 });
     }
 
     const newTheme = await prisma.theme.create({
-      data: { name },
+      data: { name, originalPrice, discount },
     });
 
     return NextResponse.json(newTheme, { status: 201 });
