9c0617180b3f19f120d8f0ae345c95c4bbf22185
azhmifauzimahdin <azhmifauzi11@gmail.com>
2025-08-21 23:00:36 +0700

    repair: classname luxury

--- components/template/luxury/luxury2.tsx
+++ components/template/luxury/luxury2.tsx
@@ -146,7 +146,7 @@ const Luxury2Page: React.FC<Invitation> = (initialInvitation) => {
           {/* ====== Hero Section ======*/}
           <section
             id="hero"
-            className="flex-center min-h-screen-svh overflow-hidden relative"
+            className="flex-center h-screen-svh overflow-hidden relative"
           >
             {/* Konten utama */}
             <div className="flex flex-col items-center justify-center text-center gap-3 relative z-20 w-full">
